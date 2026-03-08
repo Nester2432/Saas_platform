96dbaa330a6701e78839fa7933e615a4086e4178
Nester2432 <kevin.maimbil.k@gmail.com>
2026-03-08 16:19:21 -0300

    Create __init__.py for turnos services package
    Add public re-exports for the turnos services package.

---
+++ modules/turnos/services/__init__.py
@@ -0,0 +1,29 @@
+"""
+modules/turnos/services/__init__.py
+
+Public re-exports for the turnos services package.
+
+Views and external callers import from this module only:
+    from modules.turnos.services import DisponibilidadService, TurnoService
+
+Internal structure (disponibilidad.py, turnos.py) is an implementation detail.
+"""
+
+from modules.turnos.services.disponibilidad import (
+    DisponibilidadService,
+    ResultadoDisponibilidad,
+    SlotDisponible,
+    MOTIVO_FUERA_DE_HORARIO,
+    MOTIVO_BLOQUEO_ACTIVO,
+    MOTIVO_TURNO_EXISTENTE,
+)
+
+__all__ = [
+    "DisponibilidadService",
+    "ResultadoDisponibilidad",
+    "SlotDisponible",
+    "MOTIVO_FUERA_DE_HORARIO",
+    "MOTIVO_BLOQUEO_ACTIVO",
+    "MOTIVO_TURNO_EXISTENTE",
+    # "TurnoService",  ← added when turnos.py is implemented
+]
