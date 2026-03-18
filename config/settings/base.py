44f9e46a3cd1bab4af420f6f34c6a68b5c87b910
Nester2432 <kevin.maimbil.k@gmail.com>
2026-03-18 17:43:06 -0300

    fix: 401 unauthorized in crm by enabling session auth and adding csrf headers

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,6 +113,7 @@ CACHES = {}
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
